90b7e21054acea1d48a37761caa6b24434114e2b
RaviJoshi674 <131613666+RaviJoshi674@users.noreply.github.com>
2026-03-26 00:58:28 +0530

    add user registration route with file upload support

---
+++ src/routes/user.routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {upload} from "../middlewares/multer.middleware.js";
+import { registerUser } from "../controller/user.controller.js";
+
+const router=express.Router();
+
+router.route("/register").post(
+    upload.fields([
+        { name:"avatar", maxCount : 1 },
+        { name:"coverImage", maxCount : 1 }
+    ]),
+    registerUser
+
+);
+
+export default router;
