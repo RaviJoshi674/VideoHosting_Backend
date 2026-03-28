cd2a39f3842f92c3249e0c92e495fd7abf256f2e
RaviJoshi674 <131613666+RaviJoshi674@users.noreply.github.com>
2026-03-29 02:08:39 +0530

    feat: add refresh token functionality to renew access tokens and update user routes

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,6 +3,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshToken = async(userId)=>{
     try {
@@ -172,7 +173,50 @@ const logoutUser = asyncHandler(async(req,res)=>{
     .json(new ApiResponse(200, {}, "User logged out"))
 })
 
-export {registerUser, loginUser, logoutUser}
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+
+    const incomingRefreshToken = req.body.refreshToken || req.cookie.refreshToken;
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Unauthorized request");
+    }
+
+    try{
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+        const user = await User.findById(decodedToken?._id);
+
+        if(!user){
+            throw new ApiError(401, "Invalid refresh token");
+        }
+
+        if(incomingRefreshToken !== user?.refreshToken){
+            throw new ApiError(401, "Refresh token is expired or used")
+        }
+
+        const {accessToken, newRefreshToken} = await generateAccessAndRefreshToken(user._id)
+
+        const options={
+            httpOnly : true,
+            secure : true
+        }
+
+        res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("newRefreshToken", newRefreshToken, options)
+        .json(
+            new ApiResponse(200, 
+                {accessToken, refreshToken: newRefreshToken}, 
+                "Access token refreshed")
+        )
+
+    }
+    catch(err){
+        throw new ApiError(401, error?.message || "Invalid refresh token");
+    }
+})
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken}
 
 
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {upload} from "../middlewares/multer.middleware.js";
-import { loginUser, logoutUser, registerUser } from "../controller/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controller/user.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router=express.Router();
@@ -18,5 +18,6 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
