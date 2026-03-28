a21f20eb2bdaa06bf91be060c43f33aa17be4d44
RaviJoshi674 <131613666+RaviJoshi674@users.noreply.github.com>
2026-03-29 01:32:00 +0530

    fix: update loginUser to require either username or email and correct User model reference in auth middleware

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -112,7 +112,7 @@ const loginUser = asyncHandler(async(req,res)=>{
 
     const {email, username, password} = req.body
 
-    if(!username || !email){
+    if(!username && !email){
         throw new ApiError(400, "username or email is required")
     }
 
@@ -132,7 +132,7 @@ const loginUser = asyncHandler(async(req,res)=>{
 
     const {accessToken, refreshToken} = await generateAccessAndRefreshToken(user._id);
 
-    const loggedInUser = await user.findById(user._id).select("-password -refreshToken");
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
 
     const options = {
         httpOnly : true,
